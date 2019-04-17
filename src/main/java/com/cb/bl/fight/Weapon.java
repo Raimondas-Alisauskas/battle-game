ddba43a541905ac25bd2f6b11139744f03ac0bcb
RaimondasA <ra.alisa.123@gmail.com>
2019-04-17 14:00:08 +0300

    Arena Draft ra2

---
+++ src/main/java/com/cb/bl/fight/Weapon.java
@@ -0,0 +1,14 @@
+package com.cb.bl.fight;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Weapon {
+    private String name;
+    private int power;
+
+}
---
+++ src/main/java/com/cb/utils/fightUtils/ControllerUtility.java
@@ -0,0 +1,25 @@
+package com.cb.utils.fightUtils;
+
+import com.cb.bl.fight.Weapon;
+import com.cb.constants.AttackType;
+
+public class ControllerUtility {
+
+    public Weapon getWeaponFromAttribute(String string){
+        String[] arr = string.split(":");
+        return  new Weapon(arr[0],Integer.getInteger(arr[1]));
+    }
+
+    public AttackType getAttackTypeFromString (String string){
+        switch (string){
+            case "at":
+                return AttackType.ATTACK;
+            case "def":
+                return AttackType.DEFENCE;
+            default:
+                return AttackType.NEUTRAL;
+        }
+    }
+
+
+}
