b394994144b058c5283dededa4215113c47070cf
Aiste <aistutja@gmail.com>
2019-04-19 12:46:32 +0300

    Updated shop as of 04.19

--- src/main/java/com/cb/controllers/ShopController.java
+++ src/main/java/com/cb/controllers/ShopController.java
@@ -1,13 +1,18 @@
 package com.cb.controllers;
 
+import com.cb.bl.UserBL;
 import com.cb.dto.DefaultDTO;
 import com.cb.services.dbService.dbServiceImpl.UserDBServiceImpl;
 import com.cb.services.mapService.iMapService.ShopService;
+import com.cb.services.mapService.iMapService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
 
 
 @Controller
@@ -19,21 +24,25 @@ public class ShopController {
     @Autowired
     ShopService shopService;
 
+    @Autowired
+    UserService userService;
+
     @RequestMapping(method = RequestMethod.GET, value = "/shop")
-    public String getWeaponsTable(Model m) {
+    public String getWeaponsTable(Model m, HttpServletRequest req) {
         DefaultDTO defaultDTO = shopService.getWeapons();
         m.addAttribute("weaponsListBL", defaultDTO);
+
+        HttpSession userSession = req.getSession();
+        int userid = (int) userSession.getAttribute("id");
+        String username = (String) userSession.getAttribute("userName");
+        m.addAttribute("username", username);
+
+       System.out.println(userid);
+       System.out.println(username);
+
         return "shop";
     }
 
 
-//    @RequestMapping(method = RequestMethod.GET, value = "/shop")
-//    public String arena() {
-//        List<String> weapons = userDAL.getField();
-//        for(String s : weapons){
-//            System.out.println(s);
-//        }
-//        return "shop";
-//    }
 
 }
