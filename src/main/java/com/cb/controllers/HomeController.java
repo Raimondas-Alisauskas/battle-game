124ed8006cab350b58004341b0a3e7e461fcdc96
Artiom171 <artiom201097@gmail.com>
2019-04-24 18:26:31 +0300

    added message, when email is good, but password don't match

--- src/main/java/com/cb/controllers/HomeController.java
+++ src/main/java/com/cb/controllers/HomeController.java
@@ -3,13 +3,22 @@ package com.cb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpSession;
+
 
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public String home() {
-        return "index";
+    public String home(HttpServletRequest req) {
+        HttpSession userSession = req.getSession();
+
+        if(userSession.isNew()) {
+            return "index";
+        } else {
+            return null;
+        }
     }
 
 }
--- src/main/java/com/cb/controllers/LoginController.java
+++ src/main/java/com/cb/controllers/LoginController.java
@@ -12,6 +12,7 @@ import com.cb.services.mapService.iMapService.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -40,12 +41,14 @@ public class LoginController {
     @RequestMapping(value = "/loginuser", method = RequestMethod.POST)
     public String loginUser(HttpServletRequest req, Map<String, String> model, @ModelAttribute("userDAL") UserBL userBL, Model m) {
         int userExist = userService.getUserByEmailAndPassword(userBL);
+        int emailExist = userService.getUserByEmail(userBL);
 
         if (userExist == 1) {
             String userName = userService.getUserNameByEmail(userBL);
             int userId = userService.getUserIdByEmail(userBL);
             int fighterId = fighterService.getFighterIdByUserId(userId);
             HttpSession userSession = req.getSession();
+            assert (userSession.isNew());
             userSession.setAttribute("userName", userName);
             userSession.setAttribute("id", userId);
             userSession.setAttribute("fighterId", fighterId);
@@ -54,7 +57,7 @@ public class LoginController {
                 DefaultDTO defaultDTO = fighterService.getFighterByUserId(userId);
                 FighterBL fighterBL = (FighterBL) defaultDTO.getData();
                 m.addAttribute("fighterUser", fighterBL);
-                userSession.setAttribute("fighterImage",fighterBL.getImage());
+                userSession.setAttribute("fighterImage", fighterBL.getImage());
 
                 return "home";
 
@@ -65,19 +68,25 @@ public class LoginController {
             m.addAttribute("partiesList", partiesList);
             m.addAttribute("charactersList", charactersList);
             return "createCharacter";
-        } else if (userExist == -1) {
-            model.put("error", "User does not exist");
+        } else if (userExist == -1 && emailExist == 1) {
+            model.put("error", "Bad password, please try again");
             return "index";
         } else {
             model.put("error", "General error");
-            return "index";
+            return "errorPage";
         }
     }
 
-    @RequestMapping(value = "/signout")
-    public String signOut(HttpServletRequest req) {
-        HttpSession userSession = req.getSession();
-        userSession.invalidate();
-        return "index";
+    @GetMapping(value = "/loginuser")
+    public String refreshMyProfile() {
+
+        return "home";
     }
-}
+
+        @RequestMapping(value = "/signout")
+        public String signOut (HttpServletRequest req){
+            HttpSession userSession = req.getSession();
+            userSession.invalidate();
+            return "index";
+        }
+    }
