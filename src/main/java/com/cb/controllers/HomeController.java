d4c30342b10087d64ef2fff71fe84777da45081b
Artiom171 <artiom201097@gmail.com>
2019-04-24 18:29:42 +0300

    fixed mistakes2

--- src/main/java/com/cb/controllers/HomeController.java
+++ src/main/java/com/cb/controllers/HomeController.java
@@ -3,15 +3,11 @@ package com.cb.controllers;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpSession;
-
-
 @Controller
 public class HomeController {
 
     @RequestMapping(value = "/")
-    public String home(HttpServletRequest req) {
+    public String home() {
         return "index";
     }
 }
